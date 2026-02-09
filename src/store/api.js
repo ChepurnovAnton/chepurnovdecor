a03dcf779c19ceafe54357f67e82dbd8767cac22
ChepurnovAnton <chepurnovanton22@gmail.com>
2026-02-10 03:49:12 +0700

    change api

--- src/store/api.js
+++ src/store/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const galleryApi = createApi({
   reducerPath: "galleryApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://accessible-luck-e2f3b335dd.strapiapp.com",
+    baseUrl: "https://api.chepurnovdecor.ru",
   }),
   endpoints: (builder) => ({
     getGallery: builder.query({
